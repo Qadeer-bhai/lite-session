3d2a3cc8fe82b36b3497482fc220fb02c5181407
Qadeer-bhai <ummerkulachi@gmail.com>
2025-05-27 08:29:25 +0500

    Update pair.js

--- pair.js
+++ pair.js
@@ -105,7 +105,7 @@ router.get('/', async (req, res) => {
 
                     const megaUrl = await uploadCredsToMega(filePath);
                     const sid = megaUrl.includes("https://mega.nz/file/")
-                        ? 'TOFAN-AI~' + megaUrl.split("https://mega.nz/file/")[1]
+                        ? 'QADEER;;;' + megaUrl.split("https://mega.nz/file/")[1]
                         : 'Error: Invalid URL';
 
                     console.log(`Session ID: ${sid}`);
